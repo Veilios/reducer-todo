0ab803adf4921bac1396837ab272cfe3a2de49e3
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-16 19:05:00 -0400

    Completed the reducer

--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,5 +1,6 @@
+export const ADD_ITEM = "ADD_ITEM"
 export const TOGGLE_ITEM = "TOGGLE_ITEM";
-export const UPDATE_TODO = "UPDATE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
 
 export const initialStates = {
     item: 'Learn about reducers',
@@ -9,10 +10,28 @@ export const initialStates = {
 
 export const todoReducer = (state, action) => {
     switch (action.type) {
+        case ADD_ITEM:
+            return {
+                ...state,
+                item: action.payload, 
+                completed: false, 
+                id: Date.now()
+            };
         case TOGGLE_ITEM:
-            return {...state,};
-        case UPDATE_TODO:
-            return state;
+            return(
+              state.map(todo => {
+                if (todo.id === action.payload){
+                  return{...todo, completed: !todo.completed }
+                }
+                return todo;
+              })
+            )
+        case REMOVE_ITEM:
+            return (
+                state.filter(item => {
+                    item.id !== action.payload
+                })
+            );
         default:
             return state;
     };

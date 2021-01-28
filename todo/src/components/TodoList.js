cc2eac030b467d90bfd9e95f36f8c771b9b1ca65
Alan Mir <alanpaulphotos@gmail.com>
2021-01-27 22:24:33 -0500

    Added initialState

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Todo from './Todo';
+
+const intitialTasks = {
+    tasks = [{
+        description: "Play Video Games",
+        completed: false,
+        id: 123,
+      },
+      {
+        description: "Tell off a 10 year old in game lobby",
+        completed: false,
+        id: 456,
+      },
+      {
+        description: "Saviour the win",
+        completed: false,
+        id: 789,
+      }]
+};
 
 const TodoList = () => {
-
+    return (
+        <div>
+            {}
+        </div>
+    );
 };
 
 export default TodoList;
